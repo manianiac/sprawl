c049b4ac8dad004fc4338e96f7243504eb5490df
Will Barron <williambarronnexus@gmail.com>
2020-04-09 18:30:26 -0400

    Add Arwes to project, created sprawl file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme, Arwes, Link } from 'arwes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
+    <ThemeProvider theme={createTheme()}>
+      <Arwes>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <Link
+              href="https://reactjs.org"
+            >
+              Learn React
+        </Link>
+          </header>
+        </div>
+      </Arwes>
+    </ThemeProvider>
+
   );
 }
 
